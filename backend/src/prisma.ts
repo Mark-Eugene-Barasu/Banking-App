67dac772c90615f144cbb91ac63bed7bb8263d23
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2026-04-24 18:07:42 +0200

    updated BANKING APP

--- backend/src/prisma.ts
+++ backend/src/prisma.ts
@@ -1,6 +1,10 @@
 import { PrismaClient } from "@prisma/client";
-import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import * as dotenv from "dotenv";
 
-const prisma = new PrismaClient();
+dotenv.config();
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
